0efb1bbe95b3c0e4d1f6bffeaeed50b72a1ef3dc
Arvid Pålstedt <arvidpalstedt@Arvids-MacBook-Pro.local>
2022-02-28 10:05:39 +0100

    footer added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Product from './Product.js';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Product />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer className='footer'>
+        <div className='footer-contact-follow'>
+          <div className='footer-contact'>
+            <div><b>Contact</b></div>
+            <div>0768999596</div>
+            <div>astrid.thil@gmail.com</div>
+            <div>Luntmakargatan 78</div>
+          </div>
+        <div className='footer-follow'>
+          <div><b>Follow us</b></div>
+          <div>Facebook</div>
+          <div>Instagram</div>
+          <div>Twitter</div>
+        </div>
+      </div>
+        <div className='footer-copy'>Copyright &copy; 2022</div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
