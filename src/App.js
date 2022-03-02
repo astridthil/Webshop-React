4dc299122c28d48658d06ca704fe1c6954a6ae1f
Arvid Pålstedt <arvidpalstedt@Arvids-MBP.lan>
2022-03-02 22:11:08 +0100

    checkout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from './Product.js';
-
 import ProductPage from './ProductPage';
 import Checkout from './Checkout';
 import { useState } from 'react';
@@ -32,6 +31,7 @@ function App() {
         <li> <Link to='/products'>products</Link> </li>
       </ul>
       </BrowserRouter>
+   
       
     </div>
   );
